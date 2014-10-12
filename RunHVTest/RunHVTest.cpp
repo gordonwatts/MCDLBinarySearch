04ff3dc4eaeb46a8bed34bf13d640bf247dd11e2
Gordon Watts <gordon@gordonandpaula.net>
2014-10-12 23:57:41 +0200

    Writing the shell and making sure it builds.

---
+++ RunHVTest/RunHVTest.cpp
@@ -0,0 +1,14 @@
+//
+// Run a simple MC generation as a test. Nothing sophisticated...
+//
+
+#include "Pythia8/Pythia.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hi" << endl;
+}
