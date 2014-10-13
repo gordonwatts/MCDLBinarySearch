1a0a85d634ba32056d309a5c192f7477192e112b
Gordon Watts <gordon@gordonandpaula.net>
2014-10-13 22:51:00 +0200

    Extrat the limit info from the files

---
+++ printSearchResults.ps1
@@ -0,0 +1,9 @@
+﻿# Find all the local text files, search for the last good line, and print it...
+
+foreach ($f in $(Get-ChildItem ./*.txt)) {
+    $l = Get-Content $f | ? {$_ -match "For decay length"}
+    if ($l) {
+        $last = $l[-1]
+        Write-Host "${f}: $last"
+    }
+}
